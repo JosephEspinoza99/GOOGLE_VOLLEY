ee412bafb4994cb26865f284b2e5553716ea1467
JosephEspinoza99 <josephstevenespinozaoviedo@gmail.com>
2023-06-25 21:42:09 -0500

    HOLA

--- app/src/main/java/com/example/semana4/ListaBancos.java
+++ app/src/main/java/com/example/semana4/ListaBancos.java
@@ -3,7 +3,17 @@ package com.example.semana4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,12 +21,13 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import WebService.Asynchtask;
 import WebService.WebService;
 
-public class ListaBancos extends AppCompatActivity implements Asynchtask {
+public class ListaBancos extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +35,67 @@ public class ListaBancos extends AppCompatActivity implements Asynchtask {
         setContentView(R.layout.activity_lista_bancos);
 
 
-        Bundle bundle = this.getIntent().getExtras();
-        Map<String, String> datos = new HashMap<String, String>();
-        datos.put("fuente","1");
-        WebService ws= new WebService(" https://api.uealecpeterson.net/public/productos/search"
-                ,datos, ListaBancos.this, ListaBancos.this);
-        ws.execute("POST","Authorization","Bearer "+ bundle.getString("TOKEN"));
+        Bundle bundle = getIntent().getExtras();
+        Map<String, String> datos = new HashMap<>();
+        datos.put("fuente", "1");
+
+        String url = "https://api.uealecpeterson.net/public/productos/search";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(datos),
+                this, this) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + bundle.getString("TOKEN"));
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
 
     }
 
 
+    //@Override
+   // public void processFinish(String result) throws JSONException {
+        //TextView Informacion = (TextView)findViewById(R.id.informacion);
+        //String lstBancos="";
+        //JSONObject JSONOBJ = new  JSONObject(result);
+        //JSONArray JSONlista = JSONOBJ.getJSONArray("productos");
+        //for(int i=0; i< JSONlista.length();i++){
+            //JSONObject producto= JSONlista.getJSONObject(i);
+           // lstBancos = lstBancos +" ("+i+") "+producto.getString("barcode").toString()+" "+" "+" "+producto.getString("descripcion").toString()
+            //        +producto.getString("costo").toString()+" "+" "+" "+producto.getString("impuesto").toString()+"\n";
+
+        //}
+        //Informacion.setText(lstBancos);
+
+    //}
+
     @Override
-    public void processFinish(String result) throws JSONException {
+    public void onErrorResponse(VolleyError error) {
         TextView Informacion = (TextView)findViewById(R.id.informacion);
-        //Informacion.setText(result);
-        String lstBancos="";
-        JSONObject JSONOBJ = new  JSONObject(result);
-        JSONArray JSONlista = JSONOBJ.getJSONArray("productos");
-        for(int i=0; i< JSONlista.length();i++){
-            JSONObject producto= JSONlista.getJSONObject(i);
-            lstBancos = lstBancos +" ("+i+") "+producto.getString("barcode").toString()+" "+" "+" "+producto.getString("descripcion").toString()
-                    +producto.getString("costo").toString()+" "+" "+" "+producto.getString("impuesto").toString()+"\n";
+        Informacion.setText("ERROR");
+    }
 
+    @Override
+    public void onResponse(JSONObject response) {
+        ArrayList<String> lstBancos = new ArrayList<String>();
+        JSONArray productosArray = null;
+        try {
+            // Acceder a la matriz "productos" dentro del objeto JSON principal
+            productosArray = response.getJSONArray("productos");
+
+            for (int i = 0; i < productosArray.length(); i++) {
+                JSONObject producto = productosArray.getJSONObject(i);
+                lstBancos.add("("+i+")"+producto.getString("barcode").toString()+" "+" "+" "+producto.getString("descripcion").toString()
+                        +producto.getString("costo").toString()+" "+" "+" "+producto.getString("impuesto").toString()+"\n");
+            }
+
+            TextView txtvolley = findViewById(R.id.informacion);
+            txtvolley.setText(lstBancos.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        Informacion.setText(lstBancos);
-
     }
 }
